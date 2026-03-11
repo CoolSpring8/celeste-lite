a41ff7a6f4ff75885808414a0e301a556993060f
CoolSpring8 <coolspring888@gmail.com>
2026-03-11 14:10:47 +0800

    feat: update level design layout to form a continuous and interconnected landform

--- src/level.ts
+++ src/level.ts
@@ -5,36 +5,36 @@ import { LevelEntitySpec, SpikeDirection } from "./entities/types";
 // 0 = 空气, 1 = 实心方块
 // 这张图设计了：地面、墙壁、浮空平台、需要冲刺越过的大间隙、练习蹬墙跳的竖井
 const RAW = `
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-X..................................................X
-X..................................................X
-X..................................................X
-X..................................................X
-X..................................................X
-X..................................................X
-X........XX.............................XX.........X
-X..................................................X
-X..................................................X
-X..................................................X
-X...............................XX.................X
-X.......................XX.........................X
-X..................................................X
-X...........................2......................X
-X...........XX...........======....................X
-X.................XX..............XX.........XX....X
-X..................................................X
-X..................................................X
-X..XX.....................^........................X
-X..XX......XX..........X====X..........XXXX........X
-X..XX.............................1................X
-X..XX......................................XX.XX...X
-X..XX..............................................X
-X..XX..XX..XX..........X....X......................X
-X.................,................................X
-X1S................................................X
-X..................................................X
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`;
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+XXXXX..............................................XXXX
+XXXXX..............................................XXXX
+XXXXX..............................................XXXX
+XXXXX..............................................XXXX
+XXXXX....................XXXXXXXX..................XXXX
+XXXXX.2......^^..........XXXXXXXX..................XXXX
+XXXXX........XX..........XXXXXXXX..................XXXX
+XXXXX........XX...........XXXXXXX..................XXXX
+XXXXX........XX...........XXXXXXX......XXXXXX===XXXXXXX
+XXXXX........XX...........XXXXXXX......XXXXXX...XXXXXXX
+XXXXXXXX..................XXXXXXX........XXXX...XXXXXXX
+XXXXXXXX.........^^^^^....XXXXXXX.........XXX...XXXXXXX
+XXXXXXXX.........XXXXX....XXXXXXX.........XXX...XXXXXXX
+XXXXXXXX.........XXXXX...1XXXXXXX.........XXX...XXXXXXX
+XXXXXXXX.........XXXXX....XXXXXXX.........XXX...XXXXXXX
+XXXX......................XXXXXXX..................XXXX
+XXXX......................XXXXXXX..................XXXX
+XXXX...........XXXXXXXX...XXXXXXX..................XXXX
+XXXX...........XXXXXXXX...XXXXXXX..................XXXX
+XXXXX..........XXXXXXXX......XXXX.............=====XXXX
+XXXXX..........XXXXXXXX.....XXXXX..................XXXX
+XXXXXS.........XXXXXXXX............................XXXX
+XXXXX..........XXXXXXXX...XXXXXXX..................XXXX
+XXXXXXXXX^^^^^^XXXXXXXX^^^XXXXXXX.......XXXX.......XXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.......XXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX^^^^^^^XXXX
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`;
 
 export interface LevelData {
   world: EntityWorld;
